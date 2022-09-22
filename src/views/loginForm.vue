a042123a0513b1714eeb70f360f3d3c0e1064e3a
OdaTI-SK <72475585+OdaTI-SK@users.noreply.github.com>
2022-09-21 21:40:48 -0300

    Nova tela de login

--- src/views/loginForm.vue
+++ src/views/loginForm.vue
@@ -1,21 +1,105 @@
 <template>
-   <v-flex xs12 sm8 md4>
-      <v-card class="elevation-12">
-         <v-toolbar dark color="#5C2715" title="Login" />
+   <div class="divMain">
+      <div class="divImg">
+         <v-img height="900" src="../assets/teste4.jpg"> </v-img>
+      </div>
+      <div class="divLogin"> 
+      <v-card style="height: 100%">
+         <v-card-title style="text-align: center; margin-top: 35px">Faça login para continuar</v-card-title>
+         <v-spacer></v-spacer>
          <v-card-text>
-            <v-form>
-               <v-text-field prepend-icon="person" name="login" label="User" type="text" v-model="user"></v-text-field>
-               <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" v-model="pass">
-               </v-text-field>
+            <v-form style="margin-top: 80px; height: 100%;">
+               <div class="divInputs">
+                  <v-text-field  
+                     variant="outlined" 
+                     placeholder="Digite o usuário"
+                     prepend-inner-icon="mdi-account" 
+                     name="login"
+                     label="User" 
+                     v-model="user"
+                  >
+                  </v-text-field>
+                  <v-spacer></v-spacer>
+                  <v-text-field 
+                     variant="outlined" 
+                     placeholder="Digite a senha"
+                     label="Password"
+                     id="password" 
+                     prepend-inner-icon="mdi-lock" 
+                     name="password" 
+                     type="password" 
+                     v-model="pass"
+                   >
+                  </v-text-field>
+               </div>
             </v-form>
          </v-card-text>
-         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="#5C2715" @click="loginAwait()">Login</v-btn>
-         </v-card-actions>
+            <div class="btnLogin">
+            <v-btn style="width: 70%; height: 45px; color: white" color="#5C2715" @click="loginAwait()">Login</v-btn>
+            </div>
+             
+          <div class="footer">
+            <span>
+               Copyright (c) 2022 Ricardo Jeferson Hendges
+            </span>
+             <v-spacer></v-spacer>
+            <span>
+            Designer: Odair Sehn
+            </span>
+         </div>
       </v-card>
-   </v-flex>
+      </div>
+      <template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="600"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+         Altere sua senha para o primeiro acesso!
+        </v-card-title>
+        <v-card-text>
+                <v-text-field 
+                  variant="outlined" 
+                  label="Digite a senha"
+                  id="password" 
+                  prepend-inner-icon="mdi-lock" 
+                  name="password" 
+                  type="password" 
+                  v-model="newPass"
+               >
+               </v-text-field>
+               <v-text-field 
+                  variant="outlined" 
+                  label="Repita a senha"
+                  id="password" 
+                  prepend-inner-icon="mdi-lock" 
+                  name="password" 
+                  type="password" 
+                  v-model="newPass"
+               >
+               </v-text-field>
+               </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Confirmar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
+</div>
+</template>
+
 
 <script>
 import { mapActions } from 'vuex'
@@ -24,7 +108,9 @@ export default {
    data() {
       return {
          user: null,
-         pass: null
+         pass: null,
+         dialog: false,
+         newPass: null
       }
    },
    methods: {
@@ -36,3 +122,39 @@ export default {
    }
 }
 </script>
+
+<style scoped>
+.divMain {
+   display: flex;
+   width: 100%;
+   height: 655px;
+}
+.divImg {
+   width: 60%;
+   height: 100%;
+   background: #0b0e17;
+}
+.divLogin {
+   width: 40%;
+   height: 100%;
+}
+.divInputs {
+   display: inline-flexbox;
+   align-items: center;
+   justify-content: center;
+   width: 95%;
+}
+.btnLogin {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin-top: 40px;
+}
+.footer {
+   position: relative;
+   top: 158px;
+   margin-left: 10px;
+   color: rgb(97, 92, 92);
+   font-size: 15px;
+}
+</style>
